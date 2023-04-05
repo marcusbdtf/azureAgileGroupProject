a4966524b522c42114605827dbaf55f10ab39a25
srwa.farkha@hotmail.com <srwa.farkha@hotmail.com>
2023-04-05 18:09:26 +0200

    Added Web api server

---
+++ TeamKville/Server/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamKville.Server.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
