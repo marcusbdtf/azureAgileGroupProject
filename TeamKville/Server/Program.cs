52470a47bf403380dd03856511ab660e8f4cedcf
srwa.farkha@hotmail.com <srwa.farkha@hotmail.com>
2023-04-05 11:37:54 +0200

    Mapp struktur

--- TeamKville/Server/Program.cs
+++ TeamKville/Server/Program.cs
@@ -1,6 +1,14 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using TeamKville.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
