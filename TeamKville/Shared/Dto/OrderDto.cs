6b5fa894481892c674e9c47630b2886150a652bb
Kerko0 <eric.kjelldorff@telia.com>
2023-04-06 11:29:35 +0200

    Products can be viewed. No functionality for recommended products yet.

--- TeamKville/Shared/Dto/OrderDto.cs
+++ TeamKville/Shared/Dto/OrderDto.cs
@@ -3,7 +3,6 @@
 public class OrderDto
 {
 	public int OrderId { get; set; }
-	public IEnumerable<ProductQuantity> OrderProducts { get; set; } = new List<ProductQuantity>();
 	public Guid UserId { get; set; }
 	public DateTime OrderDate { get; set; }
 	public bool Status { get; set; }
