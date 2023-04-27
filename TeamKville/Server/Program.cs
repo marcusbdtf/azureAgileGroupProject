7c828adae5f9331bb11f29baad90505d1c998cd8
Kerko0 <eric.kjelldorff@telia.com>
2023-04-27 10:43:02 +0200

    API är säkrade med en randomly generated GUID.

--- TeamKville/Server/Program.cs
+++ TeamKville/Server/Program.cs
@@ -5,6 +5,8 @@ using TeamKville.Server.Data;
 using TeamKville.Server.Data.DataModels;
 using TeamKville.Server.Data.Repositories;
 using TeamKville.Server.Data.Repositories.Interfaces;
+using TeamKville.Server;
+using TeamKville.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +14,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(option =>
 {
-	option.UseSqlServer("Server=tcp:teamkville.database.windows.net,1433;Initial Catalog=teamkville-db;Persist Security Info=False;User ID=teamkville;Password=morotärgott123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-	//option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+	option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"));
 
 });
 
-builder.Services.AddControllers();
+SharedClass.connectionStringBlob = builder.Configuration.GetConnectionString("connectionStringBlob");
+
+
+builder.Services.AddControllers(options =>
+	options.Filters.Add<ApiKeyAttribute>());
+
+//Kommentera ovan och kommentera ut den nedanför för att testa i swagger. 
+//builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
